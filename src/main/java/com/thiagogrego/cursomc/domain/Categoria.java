7890adcea2db71531399ad4e350f04d8d6c05a14
Grego <thiago.t.grego@accenture.com>
2022-01-28 21:43:45 -0300

    Banco de dados H2

--- src/main/java/com/thiagogrego/cursomc/domain/Categoria.java
+++ src/main/java/com/thiagogrego/cursomc/domain/Categoria.java
@@ -3,9 +3,17 @@ package com.thiagogrego.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @SuppressWarnings("serial")
+@Entity
 public class Categoria implements Serializable {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
