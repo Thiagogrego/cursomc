32a5c0cf7d53e835f3d08b49f2e175a63c1c5e69
Grego <thiago.t.grego@accenture.com>
2022-01-30 20:53:07 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/thiagogrego/cursomc/domain/Pedido.java
+++ src/main/java/com/thiagogrego/cursomc/domain/Pedido.java
@@ -17,7 +17,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @SuppressWarnings("serial")
 @Entity
@@ -32,12 +31,10 @@ public class Pedido implements Serializable {
 	
 
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
-	@JsonManagedReference
 	private Pagamento pagamento; 
 	
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
-	@JsonManagedReference
 	private Cliente cliente;
 	
 	@ManyToOne
