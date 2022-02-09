2757fba3b97c9ce004c00c2c1022386610e90f97
Grego <thiago.t.grego@accenture.com>
2022-02-09 12:09:40 -0300

    Refresh token

---
+++ src/main/java/com/thiagogrego/cursomc/resources/AuthResource.java
@@ -0,0 +1,30 @@
+package com.thiagogrego.cursomc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thiagogrego.cursomc.security.JWTUtil;
+import com.thiagogrego.cursomc.security.UserSS;
+import com.thiagogrego.cursomc.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@PostMapping("/refresh_token")
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+
+}
